ba5cfc6ce9023da535872d7a2bb50194dd7aa88e
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-07-23 14:59:59 -0400

    routing

---
+++ S26Week11RoutingLayoutsForms/Models/ProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace S26Week11RoutingLayoutsForms.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Price { get; set; }
+    }
+}
