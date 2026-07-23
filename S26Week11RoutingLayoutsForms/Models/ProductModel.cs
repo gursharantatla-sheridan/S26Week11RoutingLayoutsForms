255c5d890ecb0e66899106a715358624febcd71d
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-07-23 16:07:25 -0400

    complete

--- S26Week11RoutingLayoutsForms/Models/ProductModel.cs
+++ S26Week11RoutingLayoutsForms/Models/ProductModel.cs
@@ -1,9 +1,15 @@
-﻿namespace S26Week11RoutingLayoutsForms.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace S26Week11RoutingLayoutsForms.Models
 {
     public class ProductModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
         public string? Name { get; set; }
+
+        [Range(0.01, 10000, ErrorMessage = "Price must be greater than 0")]
         public double Price { get; set; }
     }
 }
